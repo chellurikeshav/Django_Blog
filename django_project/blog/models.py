cdfb7661ff1d2b76450424d872bbddd01b6407f8
chellurikeshav <chelluri.keshav@gmail.com>
2021-05-27 18:18:51 +0530

    Updating Files

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils import timezone
-from django.urls import reverse
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
